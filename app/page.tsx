4942830e4e59eee9a8fa4f74bc12d677d6459b79
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-08 12:37:44 +0100

    testimonials completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import { navBarItems } from "@/data";
 import Projects from "@/components/Projects";
+import Clients from "../components/Clients";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <Projects />
+        <Clients />
       </div>
     </main>
   );

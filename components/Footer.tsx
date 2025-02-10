20c18537d0cbddd9a65cf05b28592fdc384a83a0
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-10 15:55:42 +0100

    footer completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
@@ -11,7 +12,7 @@ const Footer = () => {
           className="w-full h-full opacity-50"
         />
       </div>
-      <div className="flex -flex-col items-center">
+      <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
@@ -27,6 +28,21 @@ const Footer = () => {
             position="right"
           />
         </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Stefan Kostic
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <div
+              key={info.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );

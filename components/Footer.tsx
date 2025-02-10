a8aa1a95fcbc620332a4ef48f708da7625ded598
StefanKostic88 <stefankostic88@yahoo.com>
2025-02-10 17:52:28 +0100

    fix 1.0.1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96 hidden md:block">
         <img
           src="/footer-grid.svg"
           alt="grid"
